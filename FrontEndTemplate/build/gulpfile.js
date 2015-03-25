e277e9941011e65b7bcc9aec1945dbb655f754a1
Jonathan Beech <jon@leftandright.co.za>
2015-03-25 16:12:01 +0200

    Added gulp less & watch support

--- FrontEndTemplate/build/gulpfile.js
+++ FrontEndTemplate/build/gulpfile.js
@@ -1,5 +1,26 @@
-var gulp = require('gulp');
+var gulp 		= require("gulp"),
+	less 		= require("gulp-less"),
+	minifyCSS 	= require("gulp-minify-css"),
+	watch 		= require("gulp-watch");
 
-gulp.task('default', function() {
-	console.log(">>> hello there this is the default task running :)")
+// Change the directory to the web root
+process.chdir("../src/");
+
+// Run all the less compilations
+gulp.task("less", function() {
+	console.log(">>> hello there this is the default task running :)");
+
+	gulp
+		.src("less/**/*.less")
+		.pipe(less())
+		.pipe(gulp.dest("css/"));
 });
+
+gulp.task("watch", function(){
+	watch("less/**/*.less", function () {
+		gulp.run("less");
+	})
+});
+
+
+gulp.task("default", ["less"]);
