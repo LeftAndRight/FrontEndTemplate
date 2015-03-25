46db3f799bc22e87fbe40751c7f466c82caef0df
Jonathan Beech <jon@leftandright.co.za>
2015-03-25 21:16:16 +0200

    Updated system to use grunt

---
+++ FrontEndTemplate/build/Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+	// Load the plugins
+	grunt.loadNpmTasks("grunt-contrib-less");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+
+	// Change to the web-root
+	grunt.file.setBase("../src");
+
+	// Project configuration.
+	grunt.initConfig({
+		less : {
+			development: {
+				files: [{
+					expand	: true,
+					cwd		: "less/",
+					src		: ["**/*.less"],
+					dest	: "css/",
+					ext		: ".css"
+				}]
+			}
+		},
+		watch : {
+			files 	: ["less/**/*.less"],
+			tasks	: ["less"],
+			options: {
+				spawn: false
+			}
+		}
+	});
+
+	// Default task(s).
+	grunt.registerTask("default", ["less"]);
+};
