9d070bf14b85ddbb1b4bd90820f68f0a7490a9b8
Jonathan Beech <jon@leftandright.co.za>
2015-05-06 10:32:15 +0200

    Removed jx rivets & rootclass

--- JXRivets/src/js/index.js
+++
@@ -1,7 +0,0 @@
-define(function(){
-	console.log("index page javascript loaded");
-
-	console.log("is jquery available globally: ", $);
-	console.log("is underscore available globally: ", _);
-	console.log("is bootstrap available globally: ", $("<div></div>").modal);
-});
